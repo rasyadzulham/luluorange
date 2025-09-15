34acf92faf45f18bdf27ec1e51dbd88c99ddf6b4
rasyadzulham <rasyad2712@gmail.com>
2025-09-15 18:51:52 +0700

    Add return data in xml and json (based on id)

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, add_product, show_product
+from main.views import show_main, add_product, show_product, show_xml, show_json, show_json_by_id, show_xml_by_id
 
 app_name = 'main'
 
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('add-product/', add_product, name='add_product'),
     path('product/<str:id>/', show_product, name='show_product'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:product_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:product_id>/', show_json_by_id, name='show_json_by_id'),
 ]
