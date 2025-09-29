3b1295e5342de3a9b4c81998c11988c514134e97
rasyadzulham <rasyad2712@gmail.com>
2025-09-29 08:28:48 +0700

    feat: add edit and delete product
    feat: add css styling to register, login, and product details

--- main/models.py
+++ main/models.py
@@ -7,18 +7,19 @@ class Product(models.Model):
         ('atasan', 'Atasan'),
         ('bawahan', 'Bawahan'),
         ('sepatu', 'Sepatu'),
+        ('bola', 'Bola'),
         ('aksesoris', 'Aksesoris'),
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    price = models.IntegerField(default=0)
+    price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
     is_featured = models.BooleanField(default=False)
-    rating = models.FloatField(default=0)
+    rating = models.FloatField()
     
     def __str__(self):
         return self.name
